ffc27c89eb53cee027eb2fc1e3db7726dcb19b73
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-11-23 10:37:45 +0100

    revealing sections on scroll implemented using intersection api

--- script.js
+++ script.js
@@ -211,3 +211,26 @@ headerObserver.observe(header);
 // // objects for observer
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(sectionOne);
+
+/////////////////////////////////////////////////////////
+//////////////////// Reveal Sections ////////////////////
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry); //target
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15, //set it something greater than 0, section is only revealed when it is 15 % visible.
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
